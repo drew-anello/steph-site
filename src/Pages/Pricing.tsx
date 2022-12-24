994cfc69c0c6652696568ea0fa531b011acd3c92
Drew Anello <drew.anello@gmail.com>
2022-12-24 00:03:14 -0500

    need new flyer

--- src/Pages/Pricing.tsx
+++ src/Pages/Pricing.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Pricing() {
   return (
-    <div>Pricing</div>
+    <div className='pricing'>
+      <img src='src/imgs/pricelist.png' alt='pricing of services 15min-$15, 30min-$20, 45min-$25 1hr-$30'/>
+    </div>
   )
 }
 
