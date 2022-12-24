4fb5d22787a7fdaa232ed75d3a29e4db6d856a6c
Drew Anello <drew.anello@gmail.com>
2022-12-23 21:08:56 -0500

    imports bootstap and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './Components/Nav'
+import Home from './Pages/Home'
+import Pricing from './Pages/Pricing'
+import Contact from './Pages/Contact'
 
 function App() {
   return (
     <div>
-      <h1>Hey</h1>
+      <Nav />
+      <Routes>
+        <Route  path="/" element={<Home />}></Route>
+        <Route  path="/pricing" element={<Pricing />}></Route>
+        <Route  path="/contact" element={<Contact />}></Route>
+        
+      </Routes>
     </div>
   )
 }
